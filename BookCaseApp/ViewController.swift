2b5e050987423ae20ae96967b97aa5be3bfe5c95
Kirill Khudyakov <hudiakov@gmail.com>
2018-04-28 18:26:09 +0300

    Initial Commit

---
+++ BookCaseApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BookCaseApp
+//
+//  Created by Kirill Khudiakov on 28.04.2018.
+//  Copyright © 2018 Kirill Khudiakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
