6f52bdc96bc7566a88d4a1533828ee44b3e9aa46
Kirill Khudyakov <hudiakov@gmail.com>
2018-05-11 15:46:20 +0300

    Add Tablable protocol
    Get count

---
+++ BookCaseApp/Protocols/Tablable.swift
@@ -0,0 +1,15 @@
+//
+//  Tablable.swift
+//  BookCaseApp
+//
+//  Created by Kirill Khudiakov on 11.05.2018.
+//  Copyright © 2018 Kirill Khudiakov. All rights reserved.
+//
+
+import Foundation
+
+protocol Tablable {
+    
+    func getFiltredCount() -> Int
+    
+}
